074fbea2f278f0d22776c859258021a52e0adfbe
MadhanprasanthM <muthumadhan2005@gmail.com>
2025-02-19 16:28:47 +0530

    by Selva

--- Adminstration.php
+++ Adminstration.php
@@ -41,7 +41,7 @@ include("./reuseable_files/header.html");
             "Welcome to Thiruthangal Nadar College, where academic excellence meets innovation and leadership. 
             Our mission is to nurture talent, foster critical thinking, and prepare students for the dynamic world."
         </p>
-        <p class="mt-2 font-semibold text-gray-900">📌 Dr. [Devi] <br> Principal</p>
+        <p class="mt-2 font-semibold text-gray-900" style="margin-top: 30px;">📌 Dr. [Devi] <br> Principal</p>
     </section>
     <br>
     <!-- Administration Team -->
@@ -51,22 +51,22 @@ include("./reuseable_files/header.html");
             
             <div class="bg-white p-5 shadow-md rounded-lg text-center">
                 <h3 class="text-lg font-semibold">📌 Principal</h3>
-                <p class="text-gray-700">Dr. [Devi]</p>
+                <p class="text-gray-700">Dr.V [Devi]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg text-center">
                 <h3 class="text-lg font-semibold">📌 Vice Principal</h3>
-                <p class="text-gray-700">Prof. [Asaithambi]</p>
+                <p class="text-gray-700">Dr.K.C [Lathithabika]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg text-center">
-                <h3 class="text-lg font-semibold">📌 Vice principal</h3>
-                <p class="text-gray-700">Mr./Ms. [Lathithabika]</p>
+                <h3 class="text-lg font-semibold">📌 IQAC </h3>
+                <p class="text-gray-700">Dr.S.B [Ninu]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg text-center">
-                <h3 class="text-lg font-semibold">📌 Controller of Examinations</h3>
-                <p class="text-gray-700">Dr. [Name]</p>
+                <h3 class="text-lg font-semibold">📌 E-Governance</h3>
+                <p class="text-gray-700">Mrs.S [Sathya]</p>
             </div>
         </div>
     </section>
@@ -74,30 +74,119 @@ include("./reuseable_files/header.html");
     <!-- HODs Section -->
     <section class="max-w-5xl mx-auto mt-10">
         <h2 class="text-2xl font-bold text-blue-800 text-center">Heads of Departments</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-            <div class="bg-white p-5 shadow-md rounded-lg">
-                <h3 class="font-semibold">✅ Computer Science</h3>
-                <p class="text-gray-700">Prof. [Name]</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
+            <div class="bg-white p-5 shadow-md rounded-1g" >
+                <h3 class="font-semibold">✅ M.Sc (Computer Science)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.A.Ambeth Raja]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
-                <h3 class="font-semibold">✅ Commerce</h3>
-                <p class="text-gray-700">Prof. [Name]</p>
+                <h3 class="font-semibold">✅ M.S.W (Social Work)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.K.Saradha Devi]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
-                <h3 class="font-semibold">✅ Management Studies</h3>
-                <p class="text-gray-700">Prof. [Name]</p>
+                <h3 class="font-semibold">✅ M.Com (General)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.B.Jagadeeswaran]</p>
+            </div>
+
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Com (General)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.E.R.Jessie Kirubarai]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
-                <h3 class="font-semibold">✅ Science (Physics, Chemistry, Mathematics)</h3>
-                <p class="text-gray-700">Prof. [Name]</p>
+                <h3 class="font-semibold">✅ B.Com (Corporate Secretaryship)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.S.Anitha]</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
-                <h3 class="font-semibold">✅ Humanities</h3>
-                <p class="text-gray-700">Prof. [Name]</p>
+                <h3 class="font-semibold">✅  B.Com (Accounting and Finance)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.R.Sharmila]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Com (Computer Application)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.T.S.Rubakannan]</p>
+            </div>
+
+            
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Com (Bank Management)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.C.Kandasamy]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Com (Information System Management)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.T.S.Rubakannan]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.B.A (Business Administration)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.B.Ravi Kumar]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.C.A (Computer Application)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.K.Somasundaram]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Computer Science)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.P.T.Kasthuri Bai]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Computer Science With AI)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.A.Yogameena]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Software Application)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.S.Lavanya]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Mathematics)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.S.Arun]</p>
+            </div>
+
+            
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Chemisry)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.C.Karthick]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Plant Biology and Plant Bio Technology)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Dr.A.Janaki]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.Sc (Criminology and Criminal Justice Science)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.S.Jayasutha]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.A (English Literature)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.K.PammeNesakumari]</p>
+            </div>
+
+            
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.A (English Literature)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.K.PammeNesakumari]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B.A (Tamil Literature)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mr.D.Sivakumar]</p>
+            </div>
+
+            <div class="bg-white p-5 shadow-md rounded-lg">
+                <h3 class="font-semibold">✅ B..S.W (Social Work)</h3>
+                <p class="text-gray-700" style="margin-top: 10px;">Prof. [Mrs.K.Saradha Devi]</p>
             </div>
         </div>
     </section>
@@ -108,40 +197,35 @@ include("./reuseable_files/header.html");
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mt-6">
             <div class="bg-white p-5 shadow-md rounded-lg">
                 <h3 class="font-semibold">📌 Academic Council</h3>
-                <p class="text-gray-700">Handles syllabus design and academic policies.</p>
+                <p class="text-gray-700" style="margin-top: 10px;">Handles syllabus design and academic policies.</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
                 <h3 class="font-semibold">📌 Discipline Committee</h3>
-                <p class="text-gray-700">Ensures student discipline and ethical conduct.</p>
+                <p class="text-gray-700" style="margin-top: 10px;">Ensures student discipline and ethical conduct.</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
                 <h3 class="font-semibold">📌 Placement & Training Cell</h3>
-                <p class="text-gray-700">Helps students with career opportunities.</p>
+                <p class="text-gray-700" style="margin-top: 10px;">Helps students with career opportunities.</p>
             </div>
 
             <div class="bg-white p-5 shadow-md rounded-lg">
                 <h3 class="font-semibold">📌 Research & Innovation Committee</h3>
-                <p class="text-gray-700">Encourages student and faculty research projects.</p>
+                <p class="text-gray-700" style="margin-top: 10px;">Encourages student and faculty research projects.</p>
             </div>
         </div>
     </section>
     <br>
-    <!-- Contact Information -->
-    <section class="max-w-4xl mx-auto  text-white text-center py-6 mt-10 rounded-lg  " style="background-color: #003366;">
-        <h2 class="text-2xl font-bold text-white" style="color:#fff">Contact Information</h2>
-        <p class="mt-4">📌 Thiruthangal Nadar College, Selavoyal, Chennai - 600051, Tamil Nadu, India.</p>
-        <p class="mt-2">📧 Email: <a href="mailto:admin@tncollege.edu" class="underline">admin@tncollege.edu</a></p>
-        <p class="mt-2">📞 Phone: +91 XXXXXXXXXX</p>
-        <p class="mt-2">🕘 Office Hours: Monday - Saturday | 9:00 AM - 5:00 PM</p>
-        </section> <br>
-<?php 
-  include("./reuseable_files/footer.html");
-  ?>     
+    
+     
   <?php 
        include('./addmission_form/admission_form.html');
         ?>
+
+<?php 
+  include("./reuseable_files/footer.html");
+  ?> 
     <script src="script/addmission.js"></script>
     <script src="script/iqac.js"></script>
 <script src="script/fixed-nav.js"></script>  
